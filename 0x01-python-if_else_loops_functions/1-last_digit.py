8f15126b7b55e4a5b4d6b50003c55d1331289a36
tg4553t <tigistse@example.com>
2024-02-12 22:30:48 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+
+print("Last digit of", number, "is", last_digit, "and", end=" ")
+
+if last_digit > 5:
+    print("is greater than 5")
+elif last_digit == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
